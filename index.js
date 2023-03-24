3025ee90543fd4c0215a00310136066c1ed17a5f
Eli Morris <elimorris@Elis-MBP.lan>
2023-03-23 21:48:02 -0700

    starting index.js code, looking through instructions, and setting up gitignore file

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require("inquirer");
+const fs = require('fs');
+const util = require("util");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: "input",
+    message: "What is the title of the project?",
+    name: "Title"
+}, {
+    type: "input",
+    message: "What is the project about? (Detailed description)",
+    name: "Description"
+
+}, {
+    type: "input",
+    message: "Table of Contents",
+    name: "Table of Contents"
+}, {
+    type: "input",
+    message: "What does the user need to install to run this app?",
+    name: "Insatllation"
+}
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
