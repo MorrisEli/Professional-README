cea7bddae90104d72943d65d06bfa7a918b8f87c
Eli Morris <elimorris@Elis-MBP.lan>
2023-03-25 21:58:31 -0700

    saved file

--- index.js
+++ index.js
@@ -2,15 +2,59 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 const util = require("util");
+const generateMarkdown = require("./generateMarkdown");
 // TODO: Create an array of questions for user input
 const questions = [{
     type: "input",
     message: "What is the title of the project?",
-    name: "Title"
+    name: "Title",
+
+    validate: nameInput => {
+        if(nameInput) {
+            return true;
+        } else {
+            console.log('Enter Project Title')
+            return false;
+        }
+    }
 }, {
     type: "input",
     message: "What is the project about? (Detailed description)",
     name: "Description"
+    validate: nameInput =>
+        if (nameInput) {
+            return true;
+        } else {
+            console.log('Enter Descrpiption of your project.');
+            return false;
+        }
+    }
+},
+{
+    type: 'confirm',
+    message: 'Is there an installation process?',
+    name: 'confirmInstallation'
+},
+{
+    type: 'input',
+    message: 'Is there an installation?',
+    name: 'Installation'
+},
+{
+    type: 'input',
+    message: 'Installation instructions',
+    name: 'installationInstruction'
+    When: ({ Installation }) => {
+        if(Installation) {
+            return true;
+        } else {
+            return falsel;
+        }
+
+    }
+}
+
+
 
 }, {
     type: "input",
@@ -20,14 +64,33 @@ const questions = [{
     type: "input",
     message: "What does the user need to install to run this app?",
     name: "Insatllation"
+}, {
+    type:
+    message:
+    name: 
 }
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.write(fileName, data, error => {
+        if (error) {
+            return console.log('There was an error : ' + error);
+        }
+    })
+}
 
+
+const createReadMe = util.promisify(writeToFile);
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    try {
+        const userAnswers = await inquirer.prompt(questions);
+        const myMarkdown = generateMarkdown(userAnswers);
+        console.log(myMarkdown);
+        await createReadMe('README.md', myMarkdown);
+    }
+}
 
 // Function call to initialize app
 init();
